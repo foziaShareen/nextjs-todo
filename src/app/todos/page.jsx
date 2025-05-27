d29bec9b896f0e1d50e8e0aefe0f8c148fa16234
foziaShareen <foziashareen1@gmail.com>
2025-05-27 20:01:02 +0500

    parallel data fetching

--- src/app/todos/page.jsx
+++ src/app/todos/page.jsx
@@ -1,32 +1,33 @@
-import SlowComponent2s from "@/components/Slowcomponents2second";
-import SlowComponent3s from "@/components/Slowcomponents3second";
-import TodoItems from "@/components/todoitems";
-import { Suspense } from "react";
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
+
+const urls = [
+  "https://jsonplaceholder.typicode.com/todos?_limit=5",
+  "https://procodrr.vercel.app/?sleep=2000",
+  "https://procodrr.vercel.app/?sleep=3000",
+];
 
 const Todos = async () => {
+  const [todos, data2s, data3s] = await Promise.all(
+    urls.map((url) => fetchData(url))
+  );
+
   return (
     <>
       <h1>Todos</h1>
-      <Suspense
-        fallback={
-          <div className="todos-container">
-            {Array.from({ length: 5 }).map((_, index) => (
-              <li key={index} className="shimmer">
-                <div className="shimmer-checkbox"></div>
-                <div className="shimmer-text"></div>
-              </li>
-            ))}
+      <div className="todos-container">
+        {todos.map(({ id, title, completed }) => (
+          <div className="todo-item" key={id}>
+            <input type="checkbox" checked={completed} readOnly />
+            <p>{title}</p>
           </div>
-        }
-      >
-        <TodoItems />
-      </Suspense>
-      <Suspense fallback={<div>Loading data 1</div>}>
-        <SlowComponent2s />
-      </Suspense>
-      <Suspense fallback={<div>Loading data 2</div>}>
-        <SlowComponent3s />
-      </Suspense>
+        ))}
+      </div>
+      <div>{JSON.stringify(data2s)}</div>
+      <div>{JSON.stringify(data3s)}</div>
     </>
   );
 };
